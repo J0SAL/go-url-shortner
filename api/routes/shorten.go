b230cd6691175bfbb2c0ffa025112db2aad93959
J0SAL <joyalmeida321@gmail.com>
2024-07-20 03:34:23 +0530

    added shorten.hgo and helpers

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -2,6 +2,10 @@ package routes
 
 import (
 	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/j0sal/go-url-shortner/helpers"
+	"gopkg.in/DataDog/dd-trace-go.v1/contrib/gofiber/fiber.v2"
 )
 
 type request struct {
@@ -15,4 +19,29 @@ type response struct {
 	Expiry         time.Duration `json:"expiry"`
 	XRateRemaining int           `json:"rate_limit"`
 	XRateLimitRest time.Duration `json:"rate_limit_reset"`
+}
+
+func ShortenURL(c *fiber.Ctx) error {
+	body := new(request)
+
+	if err := c.BodyParser(&body); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "cannot parse JSON"})
+	}
+
+	// Implement rate limiting
+
+	// Check if input is an actual URL
+
+	if !govalidator.IsURL(body.URL) {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid URL"})
+	}
+
+	// Check for domain error
+
+	if helpers.RemoveDomainError(body.URL) {
+		return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{"error": "Invalid URL - Nice try -_*"})
+	}
+	// enforce Https, SSL
+
+	body.URL = helpers.enforceHTTP(body.URL)
 }
